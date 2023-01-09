9bff46dc62f6299f828ea1374afa934c49539496
aanastasiyatuz <aanastasiyatuz@gmail.com>
2023-01-09 14:29:29 +0600

    debug fixed

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
