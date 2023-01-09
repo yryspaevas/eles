5c35b8f64a00bcc6df5e959a5f18d4540e7e6ad3
aanastasiyatuz <aanastasiyatuz@gmail.com>
2023-01-09 14:47:54 +0600

    post model fixed

--- main/models.py
+++ main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from account.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts')
-    title = models.CharField()
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
